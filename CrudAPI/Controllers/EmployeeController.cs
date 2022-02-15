8a0385f08dba843b7db3c796aa911fabf49e8050
Amit Shinde <AmitS13@citiustech.com>
2022-02-15 13:16:20 +0530

    commit file

---
+++ CrudAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,53 @@
+﻿using CrudAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudAPI.Controllers
+{
+    [Route("api/employee")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        public static List<Employee> empdata = new List<Employee>();
+
+        [HttpGet]
+        public List<Employee> GetEmployeeList()
+        {
+            return empdata;
+        }
+
+        [HttpGet("{id}")]
+        public Employee GetEmployeeList(int id)
+        {
+            var emp = empdata[id];
+            return emp;
+        }
+
+        [HttpPost]
+        public List<Employee> Add(Employee emp)
+        {
+            empdata.Add(new Employee(emp.Name,emp.Contact));           
+            return empdata;
+        }
+
+        [HttpPut("{id}")]
+        public List<Employee> Update(int id,Employee emp)
+        {
+           empdata[id] = emp;          
+           return empdata;
+        }
+
+        [HttpDelete("{id}")]
+        public List<Employee> Delete(int id)
+        {
+            var emp = empdata[id];
+            empdata.Remove(emp);
+            return empdata;
+        }
+    }
+}
---
+++ CrudAPI/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudAPI.Models
+{
+    public class Employee
+    {
+        public Employee(string name,string contact)
+        {
+            Name = name;
+            Contact = contact;
+        }
+        public string Name { get; set; }
+        public string Contact { get; set; }
+    }
+}
